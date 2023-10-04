558ded5047189f4c75f39a0843602ab09f5aea81
harshulsahni <reachharshul@gmail.com>
2023-10-04 18:23:52 -0400

    Add intro text editor

--- forest/src/App.jsx
+++ forest/src/App.jsx
@@ -1,53 +1,82 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
+// import reactLogo from "./assets/react.svg";
+// import { invoke } from "@tauri-apps/api/tauri";
+// import "./App.css";
+import ReactMarkdown from 'react-markdown';
+
+// function App() {
+//   const [greetMsg, setGreetMsg] = useState("");
+//   const [name, setName] = useState("");
+
+//   async function greet() {
+//     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+//     setGreetMsg(await invoke("greet", { name }));
+//   }
+
+//   return (
+//     <div className="container">
+//       <h1>Welcome to Tauri!</h1>
+
+//       <div className="row">
+//         <a href="https://vitejs.dev" target="_blank">
+//           <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+//         </a>
+//         <a href="https://tauri.app" target="_blank">
+//           <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+//         </a>
+//         <a href="https://reactjs.org" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+
+//       <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+
+//       <form
+//         className="row"
+//         onSubmit={(e) => {
+//           e.preventDefault();
+//           greet();
+//         }}
+//       >
+//         <input
+//           id="greet-input"
+//           onChange={(e) => setName(e.currentTarget.value)}
+//           placeholder="Enter a name..."
+//         />
+//         <button type="submit">Greet</button>
+//       </form>
+
+//       <p>{greetMsg}</p>
+//     </div>
+//   );
+// }
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [markdown, setMarkdown] = useState('## Type Markdown Here');
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+  function handleChange(event) {
+    setMarkdown(event.target.value);
   }
 
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
+    <div>
+      {/* <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet"/>
+      <textarea onChange={handleChange} value={markdown} />
+      <ReactMarkdown>{markdown}</ReactMarkdown> */}
+      <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet"/>
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="editor">
+        <p>Hello World!</p>
+        <p>Some initial <strong>bold</strong> text</p>
+        <p><br/></p>
       </div>
 
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
+        {var quill = new Quill('#editor', {
+          theme: 'snow'
+        });}
     </div>
   );
+
 }
 
 export default App;
